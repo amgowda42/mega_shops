ff4e2f5933524f6724c79fac7100ffe399a10ad6
Annappa Gowda <annappag2020@gmail.com>
2025-05-11 13:49:23 +0530

    Basic get Request

--- api/index.js
+++ api/index.js
@@ -3,6 +3,18 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/get", (req, res) => {
+  res.json({
+    message: "Hello from the GET endpoint!",
+    statusCode: 200,
+    success: true,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
