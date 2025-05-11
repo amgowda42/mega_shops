6893576b899f2b0c9579318631bf768ae33c6ddc
Annappa Gowda <annappag2020@gmail.com>
2025-05-11 19:20:28 +0530

    mongoDB Setup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+const mongo_db_uri = process.env.MONGO_DB_URI;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,3 +20,12 @@ app.get("/get", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+mongoose
+  .connect(mongo_db_uri)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((err) => {
+    console.log("There was an error connecting to MongoDB", err);
+  });
