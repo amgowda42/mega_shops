4353bddf5bbfc85b741e3da13c2a0fccaee5f81b
Annappa Gowda <annappag2020@gmail.com>
2025-05-12 00:17:45 +0530

    taking req from the body

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 const mongo_db_uri = process.env.MONGO_DB_URI;
 const mongoose = require("mongoose");
 
+app.use(express.json());
+
+const Product = require("./models/product.model");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -17,14 +21,18 @@ app.get("/get", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.post("/api/create", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 mongoose
   .connect(mongo_db_uri)
   .then(() => {
     console.log("Connected to MongoDB successfully!");
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
   })
   .catch((err) => {
     console.log("There was an error connecting to MongoDB", err);
