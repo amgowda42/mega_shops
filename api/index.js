f587342e777b1d7e9dab894a2ce6ed1c787e7dc7
Annappa Gowda <annappag2020@gmail.com>
2025-05-12 13:47:02 +0530

    full crud operation logic implemented

--- api/index.js
+++ api/index.js
@@ -5,10 +5,13 @@ const port = process.env.PORT || 3000;
 const mongo_db_uri = process.env.MONGO_DB_URI;
 const mongoose = require("mongoose");
 
+//middleware
 app.use(express.json());
 
+//imports
 const Product = require("./models/product.model");
 
+//get all products
 app.get("/api/products", async (req, res) => {
   try {
     const products = await Product.find();
@@ -23,7 +26,33 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-app.post("/api/create", async (req, res) => {
+//get single product
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+        statusCode: 404,
+        success: false,
+      });
+    }
+    res.status(200).json({
+      product,
+      message: "Product fetched successfully",
+      statusCode: 200,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+//create product
+app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -34,15 +63,68 @@ app.post("/api/create", async (req, res) => {
   } catch (error) {
     console.log("error", error);
     res.status(500).json({
-      error,
+      essage: error.message,
     });
   }
 });
 
+//update product
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.json({
+        message: "Product not found",
+        statusCode: 404,
+        success: false,
+      });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json({
+      updatedProduct,
+      message: "Product Updated successfully",
+      statusCode: 200,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+//delete product
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+        statusCode: 404,
+        success: false,
+      });
+    }
+    res.status(200).json({
+      message: "Product Deleted successfully",
+      statusCode: 200,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+
+//connect to mongodb
 mongoose
   .connect(mongo_db_uri)
   .then(() => {
     console.log("Connected to MongoDB successfully!");
+    //start server
     app.listen(port, () => {
       console.log(`Server is running at http://localhost:${port}`);
     });
