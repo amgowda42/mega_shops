42210047461924cbf12b5036123669ebe2aab381
Annappa Gowda <annappag2020@gmail.com>
2025-05-14 23:56:27 +0530

    Header and App page Updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router";
 import Layout from "./Layouts/Layout";
 import Product from "./features/products/Product";
+import Shops from "./features/shops/Shops";
+import AboutUs from "./Pages/AboutUs";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="shops" element={<Shops />} />
           <Route path="products" element={<Product />} />
+          <Route path="aboutus" element={<AboutUs />} />
         </Route>
       </Routes>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,42 @@
+import { NavLink } from "react-router";
 const Header = () => {
   return (
-    <div className="mx-auto border-b-2 border-b-blue-400 px-3 py-4">Hello</div>
+    <div className="mx-auto border-b-2 border-b-blue-400 px-3 py-4">
+      <div className="mx-auto max-w-2xs flex justify-between text-base font-medium">
+        <NavLink
+          to=""
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+        >
+          Home
+        </NavLink>  
+        <NavLink
+          to="shops"
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+        >
+          Shops
+        </NavLink>
+        <NavLink
+          to="products"
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="aboutus"
+          className={({ isActive }) =>
+            isActive ? "text-red-500" : "text-black"
+          }
+        >
+          About Us
+        </NavLink>
+      </div>
+    </div>
   );
 };
 
