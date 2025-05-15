d5f3d561e9b7585d332d5d8f0c4f240e688876b4
Annappa Gowda <annappag2020@gmail.com>
2025-05-16 01:00:01 +0530

    Updates in Shops feature

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router";
 const Header = () => {
   return (
-    <div className="mx-auto border-b-2 border-b-blue-400 px-3 py-4">
+    <div className="mx-auto border-b-2 border-b-blue-400 p-5">
       <div className="mx-auto max-w-2xs flex justify-between text-base font-medium">
         <NavLink
           to=""
