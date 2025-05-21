b0ef737f75b8c21a5d241ab2e9877e0b35be282f
Annappa Gowda <annappag2020@gmail.com>
2025-05-22 00:04:36 +0530

    updates in home page Designe

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,17 +1,20 @@
-// Home.jsx
-import React from "react";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className="mx-auto flex items-center justify-center text-center px-4 mt-10">
-      <div >
+      <div>
         <h1 className="text-4xl md:text-5xl font-bold text-blue-600 mb-4">
           Welcome to <span className="text-green-600">Megs Shops</span>
         </h1>
         <p className="text-gray-600 text-lg md:text-xl max-w-xl mx-auto mb-6">
           Find the best products at unbeatable prices, handpicked just for you.
         </p>
-        <button className="bg-green-600 hover:bg-green-700 text-white font-medium px-6 py-3 rounded-lg transition">
+        <button
+          className="bg-green-600 hover:bg-green-700 text-white font-medium px-6 py-3 rounded-lg transition"
+          onClick={() => navigate("/main/products")}
+        >
           Shop Now
         </button>
       </div>
