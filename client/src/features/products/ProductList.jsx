546bfa9bf9472a051aabeb0a5a808c922ccc5cd3
Annappa Gowda <annappag2020@gmail.com>
2025-06-21 02:09:53 +0530

    Updates in Product module

--- client/src/features/products/ProductList.jsx
+++ client/src/features/products/ProductList.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import ProductCard from "./ProductCard";
 import { useProductContext } from "./ProductContext";
 import ProductCardShimmer from "../../components/ui/shimmers/ProductCardShimmer";
 const ProductList = () => {
   const { products, loading } = useProductContext();
   const shimmerArray = new Array(6).fill(null);
+  const [isModelOpen, setIsModelOpen] = useState(false);
+
   return (
-    <div>
-      <div className="w-2xl mx-auto text-center">
-        <h4 className="text-red-500 text-base font-medium">Products</h4>
+    <div className="relative">
+      <div className="w-2xl mx-auto text-center space-y-3">
+        <h4 className="text-red-500 text-2xl font-medium">Products</h4>
         <h6>List of Products Available in different categories</h6>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer"
+          onClick={() => setIsModelOpen(true)}
+        >
+          Add Product
+        </button>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {loading ? (
@@ -21,6 +30,37 @@ const ProductList = () => {
           <p>Something went Wrong</p>
         )}
       </div>
+      {isModelOpen && (
+        <div className="fixed inset-0 bg-white flex justify-center items-center">
+          <div className="bg-white rounded-lg p-6 w-full max-w-md relative shadow-lg z-10">
+            <button
+              onClick={() => setIsModelOpen(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-red-500 text-2xl cursor-pointer"
+            >
+              &times;
+            </button>
+            <h3 className="text-lg font-semibold mb-4">Add Product</h3>
+            <form>
+              <label htmlFor="product" className="block mb-2 font-medium">
+                Product Name
+              </label>
+              <input
+                type="text"
+                id="product"
+                name="product"
+                className="border border-gray-300 rounded p-2 w-full mb-4"
+                required
+              />
+              <button
+                type="submit"
+                className="bg-green-600 text-white px-4 py-2 rounded"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
