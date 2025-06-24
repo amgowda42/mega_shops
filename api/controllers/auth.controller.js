e14a13e8b407a49ca33b478dcb07967221393fbc
Annappa Gowda <annappag2020@gmail.com>
2025-06-25 00:52:57 +0530

    just added login logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,3 +42,38 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found. Please register.",
+        statusCode: 404,
+        success: false,
+      });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid password",
+        statusCode: 401,
+        success: false,
+      });
+    }
+    console.log("User logged in successfully:", user);
+    res.status(200).json({
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { register } = require("../controllers/auth.controller");
+const { login } = require("../controllers/auth.controller");
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
